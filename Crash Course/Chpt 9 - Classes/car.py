522d4db8c0fdcb8c2db21e023aff6283be2967d8
Marsalis <Sitm09@gmail.com>
2020-03-07 20:08:02 -0600

    Ended on pg 177, chpt 9

--- Crash Course/Chpt 9 - Classes/car.py
+++ Crash Course/Chpt 9 - Classes/car.py
@@ -38,6 +38,12 @@ class ElectricCar(Car):
     def __init__(self, make, model, year):
         """Initializes attributes of the parent class."""
         super().__init__(make, model, year)
+        self.battery_size = 70
+
+    def describe_battery(self):
+        """Prints a statement about the battery size"""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
 
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
+my_tesla.describe_battery()
