0b83583142596d2593a16eb3e8d02cac22e1af0b
Marsalis <Sitm09@gmail.com>
2020-03-25 03:22:08 -0500

    Actually finished on page 200 'Exceptions'

---
+++ Crash Course/Chpt 10 - Files/write_message.py
@@ -0,0 +1,8 @@
+filename = 'programming.txt'
+
+with open(filename, 'a') as file_object:
+    file_object.write("\n")
+    file_object.write("I love programming\n")
+    file_object.write("I love programming more\n")
+    file_object.write("I love programming more than you\n")
+    file_object.write("I love programming more than you ever will\n")
