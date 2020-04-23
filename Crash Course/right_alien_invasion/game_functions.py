f16b0d688368de2397143b1de5c315a9dbe761b5
Marsalis <Sitm09@gmail.com>
2020-04-23 14:12:24 -0500

    Created right shooting game

---
+++ Crash Course/right_alien_invasion/game_functions.py
@@ -0,0 +1,58 @@
+import sys
+
+import pygame
+
+from bullet import Bullet
+
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
+    """Respond to keypresses."""
+    if event.key == pygame.K_UP:
+        ship.moving_up = True
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = True
+    elif event.key == pygame.K_SPACE:
+        fire_bullet(ai_settings, screen, ship, bullets)
+
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """Fire a bullet if limit not reached"""
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
+
+def check_keyup_events(event, ship):
+    """Respond to key releases"""
+    if event.key == pygame.K_UP:
+        ship.moving_up = False
+    elif event.key == pygame.K_DOWN:
+        ship.moving_down = False
+
+def check_events(ai_settings, screen, ship, bullets):
+    """Respond to keypresses and mouse events"""
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
+
+def update_screen(ai_settings, screen, ship, bullets):
+    """Update images on the screen and flip to new screen."""
+    #Redraw the screen during each pass through the loop
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+    # redraw all bullets behind ship and aliens
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+
+    # Make the most recently drawn screen visible.
+    pygame.display.flip()
+
+def update_bullets(ai_settings, screen, ship, bullets):
+    """Update position of bullets and get rid of old bullets"""
+    bullets.update()
+    # Get rid of bullets that have disappeared.
+    for bullet in bullets.copy():
+        if bullet.rect.left >= ai_settings.screen_width:
+            bullets.remove(bullet)
---
+++ Crash Course/right_alien_invasion/ship.py
@@ -0,0 +1,43 @@
+import pygame
+
+class Ship():
+
+
+    def __init__(self, ai_settings, screen):
+        """Initialize the ship and set its starting position."""
+        self.screen = screen
+        self.ai_settings = ai_settings
+
+        #Load the ship image and get its rect.
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.screen_rect = screen.get_rect()
+
+        # Start each new rocket in left center of the screen.
+        self.rect.centerx = self.rect.centerx
+        self.rect.centery = self.screen_rect.centery
+
+
+        # Store a deciaml value for the ship's center.
+        self.centerx = float(self.rect.centerx)
+        self.centery = float(self.rect.centery)
+
+        # Movement flag
+        self.moving_up = False
+        self.moving_down = False
+
+
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        # Update the ship's center value, not the rect
+        if self.moving_up and self.rect.top > 0:
+            self.centery -=self.ai_settings.ship_speed_factor
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.centery += self.ai_settings.ship_speed_factor
+
+        #Update rect object from self.center.
+        self.rect.centery = self.centery
+
+    def blitme(self):
+        """Draw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
