e365b8dbaa56527e2c32b7e8ec35110714f15ac7
Marsalis <Sitm09@gmail.com>
2020-04-23 11:20:03 -0500

    Finished Chpt 12 pg 264

--- Crash Course/alien_invasion/alien_invasion.py
+++ Crash Course/alien_invasion/alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Group
 
 from settings import Settings
 from ship import Ship
@@ -7,17 +8,22 @@ import game_functions as gf
 def run_game():
     # Initialize pygame, settings, and screen object
     pygame.init()
-    ri_settings = Settings()
+    ai_settings = Settings()
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
     # Makes the ship
     ship = Ship(ai_settings, screen)
+
+    # Make a group to store bullets in.
+    bullets = Group()
+
     # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
--- Crash Course/alien_invasion/settings.py
+++ Crash Course/alien_invasion/settings.py
@@ -10,3 +10,10 @@ class Settings():
 
         # Ship Settings
         self.ship_speed_factor = 1.5
+
+        # Bullet settings
+        self.bullet_speed_factor = 2
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 255, 0, 0
+        self.bullets_allowed = 3
