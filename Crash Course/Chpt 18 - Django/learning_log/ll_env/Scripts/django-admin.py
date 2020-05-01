be30d96949c05ed880c9757c6dd24c26ed5c0de8
Marsalis <Sitm09@gmail.com>
2020-05-01 09:07:18 -0500

    Sarted Chpt 18 and installed Django in virt env

---
+++ Crash Course/Chpt 18 - Django/learning_log/ll_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\marsalis\desktop\python learning\crash course\chpt 18 - django\learning_log\ll_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
